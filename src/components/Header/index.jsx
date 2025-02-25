b02e4cfe38cdba1ff6b73a4c5d6a62696a537b72
vineetha <vineethaveerapu4@gmail.com>
2025-02-25 20:24:15 +0100

    added phonenumber and css to that

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,16 @@
 import SocialMedia from "../SocialMedia";
 import styles from "./header.module.css";
+import phoneIcon from "../../assets/phone.svg";
 
 function Header() {
   return (
     <header className={styles.header}>
       <SocialMedia />
       <h1 className={styles.h1}>News For Today</h1>
-      <div>wip</div>
+      <div className={styles.phone}>
+        <img src={phoneIcon} alt="phone icon" />
+        <a href="tel:+46700654321">+46 700654321</a>
+      </div>
     </header>
   );
 }
